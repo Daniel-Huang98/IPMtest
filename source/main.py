caab49e498fcc872369652d7ac3f865f080b3844
Daniel Huang <Daniel.Huang4739@gmail.com>
2019-05-19 20:03:38 +1000

    successfully got an inverse transform. Now get a birds eye view with short main script

---
+++ source/main.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+image = cv2.imread("./../sampleImages/tape2.jpg")
+cv2.imshow("image",image)
+pts_src = np.array([[174, 377],[301, 44],[545, 51],[659,379]])
+pts_dst = np.array([[174, 377],[174, 0],[659, 0],[659,379]])
+h, status = cv2.findHomography(pts_src, pts_dst)
+im_out = cv2.warpPerspective(image, h, (image.shape[1],image.shape[0]))
+cv2.imshow("image1",im_out)
+cv2.resizeWindow('image', 600,600)
+cv2.waitKey(0)
