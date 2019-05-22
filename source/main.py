1db303fbc097d3f14892f93d87debbfb1d852195
Daniel Huang <Daniel.Huang4739@gmail.com>
2019-05-22 17:45:18 +1000

    added clock code

--- source/main.py
+++ source/main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-image = cv2.imread("./../sampleImages/tape2.jpg")
+image = cv2.imread("./../sampleImages/tape1.jpg")
 cv2.imshow("image",image)
 pts_src = np.array([[174, 377],[301, 44],[545, 51],[659,379]])
 pts_dst = np.array([[174, 377],[174, 0],[659, 0],[659,379]])
